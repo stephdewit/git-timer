1b12af3048573cabdc55f4c4f04b6e8e8a7d6fa1
Stéphane de Wit <contact@stephanedewit.be>
2023-07-21 00:24:33 +0200

    Search for work sessions

--- timer.go
+++ timer.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"sort"
 	"time"
@@ -8,6 +9,8 @@ import (
 	"github.com/go-git/go-git/v5"
 	"github.com/go-git/go-git/v5/plumbing/object"
 )
+
+var delay time.Duration = time.Minute * 60
 
 func main() {
 	repo, err := git.PlainOpen(".")
@@ -44,5 +47,41 @@ func main() {
 		return times[i].Before(times[j])
 	})
 
-	log.Print(times)
+	sessions := make([]result, 0, 10)
+	var current result
+	current.from = times[0]
+
+	for i := 1; i < len(times); i++ {
+		from := times[i-1]
+		to := times[i]
+
+		if to.Sub(from) < delay {
+			current.duration = to.Sub(current.from)
+		} else {
+			if current.duration > 0 {
+				sessions = append(sessions, current)
+				current = result{}
+			} else {
+				log.Print("Lonely commit")
+			}
+			current.from = to
+		}
+	}
+
+	if current.duration > 0 {
+		sessions = append(sessions, current)
+	}
+
+	for _, session := range sessions {
+		log.Print(session)
+	}
+}
+
+type result struct {
+	from     time.Time
+	duration time.Duration
+}
+
+func (r result) String() string {
+	return fmt.Sprintf("%s: %v", r.from.Format(time.RFC1123Z), r.duration)
 }
